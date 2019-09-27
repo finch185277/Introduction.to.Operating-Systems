1f69aae7d49a382cc0bcc4c23a6f4a38cee763d0
yuanchen <yuanchen@cs.nctu.edu.tw>
2019-09-27 10:07:59 +0800

    Update hw1/main.cpp
    add nullptr to last place of cv
    add exit
    check command valid

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -14,33 +14,45 @@ public server, such as a public GitHub repostiory or a public web page.
 #include <vector>
 
 int main(int argc, char *argv[]) {
-  while (true) {
+  bool is_continue = true;
+  while (is_continue) {
+    // into shell
+    std::cout << ">";
+
+    // read input line by line
+    std::string line, file;
+    std::getline(std::cin, line);
+
+    // parse the line
+    char *cv[100];  // command in char* array
+    int cv_idx = 0; // cv current index
+
+    // covert line from std::string to std::vector<char>,
+    // and use std::strtok to split command and args
+    std::vector<char> lv(line.begin(), line.end());
+    lv.push_back('\0');
+    char *token = std::strtok(&*lv.begin(), " ");
+    while (token != nullptr) {
+      cv[cv_idx++] = token;
+      token = std::strtok(nullptr, " ");
+    }
+    cv[cv_idx] = nullptr; // put a nullptr to the last place
+
+    // end the program when input "exit"
+    if (strcmp(cv[0], "exit") == 0) {
+      is_continue = false;
+      break;
+    }
+
     pid_t pid;
     pid = fork();
     if (pid == 0) {
-      std::cout << ">";
-
-      // read input line by line
-      std::string line, file;
-      std::getline(std::cin, line);
-
-      // parse the line
-      char *cv[100];                                  // command in char* array
-      int cv_idx = 0;                                 // cv current index
-      std::vector<char> lv(line.begin(), line.end()); // line in vector
-      lv.push_back('\0');
-      char *token = std::strtok(&*lv.begin(), " ");
-      while (token != nullptr) {
-        cv[cv_idx++] = token;
-        token = std::strtok(nullptr, " ");
-      }
-
       // execute the command
-      execvp(cv[0], cv);
+      if (execvp(cv[0], cv) < 0)
+        std::cout << "Command \"" << cv[0] << "\" not found!" << std::endl;
 
-      exit(0);
     } else if (pid > 0) {
-      wait(0);
+      wait(nullptr);
     } else {
       std::cout << "Error!" << std::endl;
     }
