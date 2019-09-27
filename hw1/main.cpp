f0f8af13d5d54e9a0ea16712e53c9edbd6d7d1c7
yuanchen <yuanchen@cs.nctu.edu.tw>
2019-09-27 10:57:06 +0800

    Update hw1/main.cpp
    - put child into background
    - use signal() to prevent zombie process

--- hw1/main.cpp
+++ hw1/main.cpp
@@ -26,11 +26,10 @@ int main(int argc, char *argv[]) {
     std::getline(std::cin, line);
 
     // parse the line
-    char *cv[100];  // command in char* array
-    int cv_idx = 0; // cv current index
-
     // covert line from std::string to std::vector<char>,
     // and use std::strtok to split command and args
+    char *cv[100];  // command in char* array
+    int cv_idx = 0; // cv current index
     std::vector<char> lv(line.begin(), line.end());
     lv.push_back('\0');
     char *token = std::strtok(&*lv.begin(), " ");
@@ -38,7 +37,15 @@ int main(int argc, char *argv[]) {
       cv[cv_idx++] = token;
       token = std::strtok(nullptr, " ");
     }
-    cv[cv_idx] = nullptr; // put a nullptr to the last place
+
+    // check whether run in background
+    bool is_background = false;
+    if (strcmp(cv[cv_idx - 1], "&") == 0) {
+      is_background = true;
+      cv[cv_idx - 1] = nullptr; // change "&" to nullptr
+    } else {
+      cv[cv_idx++] = nullptr; // put a nullptr to the last place
+    }
 
     // end the program when input "exit"
     if (strcmp(cv[0], "exit") == 0) {
@@ -46,15 +53,19 @@ int main(int argc, char *argv[]) {
       break;
     }
 
+    // ignore background child return signal to prevent Zombie Process
+    signal(SIGCHLD, SIG_IGN);
+
     pid_t pid;
     pid = fork();
-    if (pid == 0) {
+    if (pid == 0) { // child process
       // execute the command
       if (execvp(cv[0], cv) < 0)
         std::cout << "Command \"" << cv[0] << "\" not found!" << std::endl;
-
-    } else if (pid > 0) {
-      wait(nullptr);
+    } else if (pid > 0) { // parent process
+      // child process run in foreground, parent process should wait
+      if (!is_background)
+        wait(nullptr);
     } else {
       std::cout << "Error!" << std::endl;
     }
