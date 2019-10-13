78202726f4fc3eabbf6dc8444d264833da4b56c3
yuanchen <yuanchen@cs.nctu.edu.tw>
2019-10-13 21:41:58 +0800

    Update main.cpp

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -10,15 +10,24 @@ public GitHub repository or a public web page.
 
 #include <iostream>
 #include <sys/ipc.h>
+#include <sys/shm.h>
 #include <sys/time.h>
-#include <sys/wait.h> // wait()
-#include <unistd.h>   // fork()
+#include <sys/wait.h>
+#include <unistd.h>
 #include <vector>
 
 int main(int argc, char *argv[]) {
   int dimension, counter = 1;
   std::cin >> dimension;
+
+  // create raw matrix
+  std::vector<std::vector<int>> raw(dimension, std::vector<int>(dimension));
+  for (int i = 0; i < dimension; i++)
+    for (int j = 0; j < dimension; j++)
+      raw.at(i).at(j) = i * dimension + j;
+
   for (; counter <= 16; counter++) {
+    int checksum = 0;
     struct timeval start, end;
     gettimeofday(&start, 0);
 
@@ -28,7 +37,8 @@ int main(int argc, char *argv[]) {
     int sec = end.tv_sec - start.tv_sec;
     int usec = end.tv_usec - start.tv_usec;
     std::cout << "Multiply matrices using " << counter << " process" << '\n';
-    std::cout << "elapsed " << sec * 1000 + (usec / 1000.0) << " ms" << '\n';
+    std::cout << "elapsed " << sec * 1000 + (usec / 1000.0)
+              << " sec, Checksum: " << checksum << '\n';
   }
   return 0;
 }
