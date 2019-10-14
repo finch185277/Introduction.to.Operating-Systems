0924c3369c894f8dfdaff944bdeb6fc6623cf2c0
yuanchen <yuanchen@cs.nctu.edu.tw>
2019-10-14 13:39:37 +0800

    Update main.cpp
    - add comments

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
   int dimension;
   std::cin >> dimension;
 
-  // create the shared memory
+  // using shared memory to create three matrix
   int *matrix_a_data;
   int *matrix_b_data;
   int *matrix_c_data;
@@ -59,6 +59,7 @@ int main(int argc, char *argv[]) {
     struct timeval start, end;
     gettimeofday(&start, 0);
 
+    // fork the child process and continue
     for (int cur_p = 0; cur_p < fork_counter; cur_p++) {
       pid_t pid = fork();
       if (pid == 0) {
@@ -73,10 +74,13 @@ int main(int argc, char *argv[]) {
         exit(0);
       }
     }
+
+    // wait all child process
     for (int i = fork_counter; i > 0; i--) {
       wait(nullptr);
     }
 
+    // calculate the checksum
     for (int x = 0; x < dimension; x++) {
       for (int y = 0; y < dimension; y++) {
         checksum += matrix_c[x][y];
@@ -94,6 +98,9 @@ int main(int argc, char *argv[]) {
   }
 
   // detach and detroy the shared memory
+  free(matrix_a);
+  free(matrix_b);
+  free(matrix_c);
   shmdt(matrix_a);
   shmdt(matrix_b);
   shmdt(matrix_c);
