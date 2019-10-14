4de14854251f13ac8633657f8f5bf14177767bf0
yuanchen <yuanchen@cs.nctu.edu.tw>
2019-10-14 08:43:22 +0800

    Update main.cpp
    - cal the matrix (use shared checksum)

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -20,47 +20,61 @@ public GitHub repository or a public web page.
 int main(int argc, char *argv[]) {
   // create the shared memory
   int shmid = shmget(IPC_PRIVATE, 1024, IPC_CREAT | 0600);
-  int *shmaddr = (int *)shmat(shmid, (void *)0, 0);
+  int *checksum = (int *)shmat(shmid, (void *)0, 0);
 
-  int dimension, counter = 1;
+  int dimension;
   std::cin >> dimension;
 
   // create raw matrix
   std::vector<std::vector<int>> raw(dimension, std::vector<int>(dimension));
-  for (int i = 0; i < dimension; i++)
-    for (int j = 0; j < dimension; j++)
-      raw.at(i).at(j) = i * dimension + j;
+  for (int x = 0; x < dimension; x++)
+    for (int y = 0; y < dimension; y++)
+      raw.at(x).at(y) = x * dimension + y;
 
-  for (; counter <= 6; counter++) {
-    int checksum = 0;
+  for (int fork_counter = 1; fork_counter <= 6; fork_counter++) {
+    std::vector<std::vector<int>> final(dimension, std::vector<int>(dimension));
+    *checksum = 0;
 
     // start of count the time
     struct timeval start, end;
     gettimeofday(&start, 0);
 
-    pid_t pid = fork();
-    if (pid == 0) {
-      *shmaddr = 20;
-      std::cout << *shmaddr << '\n';
-      exit(0);
-    } else if (pid > 0) {
+    for (int cur_p = 0; cur_p < fork_counter; cur_p++) {
+      pid_t pid = fork();
+      if (pid == 0) {
+        for (int x = 0; x < dimension; x++) {
+          for (int y = 0; y < dimension; y++) {
+            if ((x * dimension + y) % fork_counter == cur_p) {
+              for (int z = 0; z < dimension; z++)
+                final.at(x).at(y) = raw.at(x).at(z) * raw.at(z).at(y);
+            }
+          }
+        }
+        exit(0);
+      }
+    }
+    for (int i = fork_counter; i > 0; i--) {
       wait(nullptr);
-      std::cout << *shmaddr + 30 << '\n';
-    } else {
-      std::cout << "Fork Error!" << '\n';
+    }
+
+    for (int x = 0; x < dimension; x++) {
+      for (int y = 0; y < dimension; y++) {
+        *checksum += final.at(x).at(y);
+      }
     }
 
     // end of count the time
     gettimeofday(&end, 0);
     int sec = end.tv_sec - start.tv_sec;
     int usec = end.tv_usec - start.tv_usec;
-    std::cout << "Multiply matrices using " << counter << " process" << '\n';
+    std::cout << "Multiply matrices using " << fork_counter << " process"
+              << '\n';
     std::cout << "elapsed " << sec * 1000 + (usec / 1000.0)
-              << " sec, Checksum: " << checksum << '\n';
+              << " sec, Checksum: " << *checksum << '\n';
   }
 
   // detach and detroy the shared memory
-  shmdt(shmaddr);
+  shmdt(checksum);
   shmctl(shmid, IPC_RMID, nullptr);
 
   return 0;
