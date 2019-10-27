9baf922c86fee5766006718acff1a204f1668ba7
finch185277 <59487andy@gmail.com>
2019-10-27 23:34:43 +0800

    Update gen_input.cpp

--- hw3/gen_input.cpp
+++ hw3/gen_input.cpp
@@ -1,13 +1,20 @@
 #include <cstdlib>
 #include <ctime>
+#include <fstream>
 #include <iostream>
 
-int main() {
-  int range;
+int main(int argc, char **argv) {
+  if (argc != 3) {
+    std::cerr << "Usage: " << argv[0] << " <file name> <# of nums>";
+    return -1;
+  }
+  std::ofstream outfile(argv[1]);
   std::srand(std::time(0));
-  std::cout << "Range: ";
-  std::cin >> range;
-  for (int i = 0; i < range; i++)
-    std::cout << std::rand() << ' ';
+  outfile << argv[2] << '\n';
+  for (int i = 0; i < std::atoi(argv[2]); i++) {
+    outfile << std::rand() << ' ';
+  }
+  outfile << '\n';
+  outfile.close();
   return 0;
 }
