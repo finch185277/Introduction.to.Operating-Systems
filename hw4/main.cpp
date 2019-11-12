c22d601a9ec7b4dbf9e62c049b5842540c2f1b0a
finch185277 <59487andy@gmail.com>
2019-11-13 00:22:39 +0800

    Update main.cpp

--- hw4/main.cpp
+++ hw4/main.cpp
@@ -20,21 +20,12 @@ public GitHub repository or a public web page.
 std::vector<sem_t> sem_up(16);
 std::vector<sem_t> sem_down(16);
 
-// MT: multiple thread
-struct MT_args {
+struct args {
   std::vector<int> *nums;
   int lb;
   int mid;
   int hb;
   int id;
-};
-
-// ST: single thread
-struct ST_args {
-  std::vector<int> nums;
-  int lb;
-  int hb;
-  int level;
 };
 
 void print_nums(std::ofstream &fst, std::vector<int> nums) {
@@ -109,91 +100,24 @@ void merge(std::vector<int> *nums, int lb, int mid, int ub) {
   }
 }
 
-void *MT_sort_l0(void *void_args) {
-  MT_args *args = (MT_args *)void_args;
-  sem_wait(&sem_down.at(1));
+void job_dispatcher() { ; }
 
-  sem_post(&sem_down.at(args->id * 2));
-  sem_post(&sem_down.at(args->id * 2 + 1));
+void sort_worker() { ; }
 
-  sem_wait(&sem_up.at(args->id * 2));
-  sem_wait(&sem_up.at(args->id * 2 + 1));
+void sort_with_n_thread(std::vector<int> &nums, n) {
+  // start of count the time
+  struct timeval st_start, st_end;
+  gettimeofday(&st_start, 0);
 
-  merge(args->nums, args->lb, args->mid, args->hb);
+  sem_post(&sem_down.at(0));
+  sem_wait(&sem_up.at(0));
 
-  std::ofstream outfile("output1.txt");
-  print_nums(outfile, args->nums);
-  outfile.close();
-
-  sem_post(&sem_up.at(1));
-  pthread_exit(nullptr);
-}
-
-void *MT_sort_l1(void *void_args) {
-  MT_args *args = (MT_args *)void_args;
-  sem_wait(&sem_down.at(args->id));
-
-  sem_post(&sem_down.at(args->id * 2));
-  sem_post(&sem_down.at(args->id * 2 + 1));
-
-  sem_wait(&sem_up.at(args->id * 2));
-  sem_wait(&sem_up.at(args->id * 2 + 1));
-
-  merge(args->nums, args->lb, args->mid, args->hb);
-
-  sem_post(&sem_up.at(args->id));
-  pthread_exit(nullptr);
-}
-
-void *MT_sort_l2(void *void_args) {
-  MT_args *args = (MT_args *)void_args;
-  sem_wait(&sem_down.at(args->id));
-
-  sem_post(&sem_down.at(args->id * 2));
-  sem_post(&sem_down.at(args->id * 2 + 1));
-
-  sem_wait(&sem_up.at(args->id * 2));
-  sem_wait(&sem_up.at(args->id * 2 + 1));
-
-  merge(args->nums, args->lb, args->mid, args->hb);
-
-  sem_post(&sem_up.at(args->id));
-  pthread_exit(nullptr);
-}
-
-void *MT_sort_l3(void *void_args) {
-  MT_args *args = (MT_args *)void_args;
-  sem_wait(&sem_down.at(args->id));
-
-  bubble_sort(args->nums, args->lb, args->hb);
-
-  sem_post(&sem_up.at(args->id));
-  pthread_exit(nullptr);
-}
-
-void ST_sort(std::vector<int> &nums, int lb, int ub, int level) {
-  if (level < 3 && lb < ub) {
-    int mid = (lb + ub) / 2;
-    ST_sort(nums, lb, mid, level + 1);
-    ST_sort(nums, mid + 1, ub, level + 1);
-    merge(nums, lb, mid, ub);
-  } else {
-    bubble_sort(nums, lb, ub);
-  }
-}
-
-void *ST_helper(void *void_args) {
-  ST_args *args = (ST_args *)void_args;
-  sem_wait(&sem_down.at(0));
-
-  ST_sort(args->nums, args->lb, args->hb, args->level);
-
-  std::ofstream outfile("output2.txt");
-  print_nums(outfile, args->nums);
-  outfile.close();
-
-  sem_post(&sem_up.at(0));
-  pthread_exit(nullptr);
+  // end of count the time
+  gettimeofday(&st_end, 0);
+  double st_sec = st_end.tv_sec - st_start.tv_sec;
+  double st_usec = st_end.tv_usec - st_start.tv_usec;
+  std::cout << "The sorting with " << n
+            << " thread(s) used: " << st_sec + (st_usec / 1000000) << " secs\n";
 }
 
 int main(int argc, char **argv) {
@@ -224,84 +148,20 @@ int main(int argc, char **argv) {
     while (ss >> num)
       nums.at(idx++) = num;
 
-    // build all thread id
-    std::vector<pthread_t> tid(16);
-
     // init all semaphore
     for (int i = 0; i < 16; i++) {
       sem_init(&sem_up.at(i), 0, 0);
       sem_init(&sem_down.at(i), 0, 0);
     }
 
-    // Multiple thread
-    std::vector<MT_args> mt_args(16);
-    std::vector<int> mt_nums(nums.begin(), nums.end());
+    // build all thread id
+    std::vector<pthread_t> tid(8);
 
-    mt_args.at(1).nums = &mt_nums;
-    mt_args.at(1).lb = 0;
-    mt_args.at(1).hb = cnt - 1;
-    mt_args.at(1).mid = (mt_args.at(1).lb + mt_args.at(1).hb) / 2;
-    mt_args.at(1).id = 1;
+    for (int i = 0; i < 8; i++)
+      pthread_create(&tid.at(i), nullptr, sort_worker, &st_args);
 
-    for (int i = 2; i <= 15; i++) {
-      mt_args.at(i).nums = &mt_nums;
-      if (i % 2) {
-        mt_args.at(i).lb = mt_args.at(i / 2).lb;
-        mt_args.at(i).hb = mt_args.at(i / 2).mid;
-      } else {
-        mt_args.at(i).lb = mt_args.at(i / 2).mid + 1;
-        mt_args.at(i).hb = mt_args.at(i / 2).hb;
-      }
-      mt_args.at(i).mid = (mt_args.at(i).lb + mt_args.at(i).hb) / 2;
-      mt_args.at(i).id = i;
-    }
-
-    for (int i = 1; i <= 15; i++) {
-      if (i == 1)
-        pthread_create(&tid.at(i), nullptr, MT_sort_l0, &mt_args.at(i));
-      else if (i == 2 || i == 3)
-        pthread_create(&tid.at(i), nullptr, MT_sort_l1, &mt_args.at(i));
-      else if (i >= 4 && i <= 7)
-        pthread_create(&tid.at(i), nullptr, MT_sort_l2, &mt_args.at(i));
-      else if (i >= 8 && i <= 15)
-        pthread_create(&tid.at(i), nullptr, MT_sort_l3, &mt_args.at(i));
-    }
-
-    // start of count the time
-    struct timeval mt_start, mt_end;
-    gettimeofday(&mt_start, 0);
-
-    sem_post(&sem_down.at(1));
-    sem_wait(&sem_up.at(1));
-
-    // end of count the time
-    gettimeofday(&mt_end, 0);
-    double mt_sec = mt_end.tv_sec - mt_start.tv_sec;
-    double mt_usec = mt_end.tv_usec - mt_start.tv_usec;
-    std::cout << "MT sorting used " << mt_sec + (mt_usec / 1000000)
-              << " secs\n";
-
-    // Single thread
-    ST_args st_args;
-    st_args.nums = nums;
-    st_args.lb = 0;
-    st_args.hb = cnt - 1;
-    st_args.level = 0;
-    pthread_create(&tid.at(0), nullptr, ST_helper, &st_args);
-
-    // start of count the time
-    struct timeval st_start, st_end;
-    gettimeofday(&st_start, 0);
-
-    sem_post(&sem_down.at(0));
-    sem_wait(&sem_up.at(0));
-
-    // end of count the time
-    gettimeofday(&st_end, 0);
-    double st_sec = st_end.tv_sec - st_start.tv_sec;
-    double st_usec = st_end.tv_usec - st_start.tv_usec;
-    std::cout << "ST sorting used " << st_sec + (st_usec / 1000000)
-              << " secs\n";
+    for (int i = 1; i <= 8; i++)
+      sort_with_n_thread(nums, i);
 
   } else { // if file not exist
     std::cout << "File: " << file_name << " does not exist!" << '\n';
