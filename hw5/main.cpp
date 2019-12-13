76d458abc4161ff3d794cc746a20a38c129f8b7a
finch185277 <59487andy@gmail.com>
2019-12-13 23:22:24 +0800

    Update main.cpp
    - add comments

--- hw5/main.cpp
+++ hw5/main.cpp
@@ -30,24 +30,32 @@ void LFU(std::vector<int> &history, int nframe) {
     auto search = pages.find(*page);
     if (search != pages.end()) {
       hit++;
+
+      // re-order current freq-list
       if (freqs.find(search->second.first)->second.first ==
           freqs.find(search->second.first)->second.second) {
+        // only one node in freq-list
         freqs.erase(search->second.first);
       } else if (search->second.second ==
                  freqs.find(search->second.first)->second.first) {
+        // node is head(MRU) of freq-list
         freqs.find(search->second.first)->second.first =
             freqs.find(search->second.first)->second.first->next;
         freqs.find(search->second.first)->second.first->prev = nullptr;
       } else if (search->second.second ==
                  freqs.find(search->second.first)->second.second) {
+        // node is tail (LRU) of freq-list
         freqs.find(search->second.first)->second.second =
             freqs.find(search->second.first)->second.second->prev;
         freqs.find(search->second.first)->second.second->next = nullptr;
       } else {
+        // node in mid of freq-list
         search->second.second->prev->next = search->second.second->next;
         search->second.second->next->prev = search->second.second->prev;
       }
 
+      // check whether upper-freq exist,
+      // if not, create new freq
       if (freqs.count(search->second.first + 1) == 0) {
         struct Node *mru = nullptr, *lru = nullptr;
         freqs.insert(std::pair<int, std::pair<struct Node *, struct Node *>>(
@@ -55,53 +63,74 @@ void LFU(std::vector<int> &history, int nframe) {
             std::pair<struct Node *, struct Node *>(mru, lru)));
       }
 
+      // add node to upper-freq
       auto freq = freqs.find(search->second.first + 1);
       search->second.second->prev = nullptr;
       search->second.second->next = nullptr;
       if (freq->second.first == nullptr && freq->second.second == nullptr) {
+        // add new node
         freq->second.first = search->second.second;
         freq->second.second = search->second.second;
       } else {
+        // add node to head (MRU)
         freq->second.first->prev = search->second.second;
         search->second.second->next = freq->second.first;
         freq->second.first = freq->second.first->prev;
       }
+
+      // update page frequency
       search->second.first++;
     } else {
       miss++;
+
+      // if frame is full
       if (pages.size() == nframe) {
+        // find the victim (lru of botten-freq)
         struct Node *victim = freqs.begin()->second.second;
 
         if (freqs.begin()->second.first == freqs.begin()->second.second) {
+          // only one node in freq
           freqs.erase(freqs.begin()->first);
         } else {
+          // update the freq
           freqs.begin()->second.second = freqs.begin()->second.second->prev;
           freqs.begin()->second.second->next = nullptr;
         }
 
+        // remove from pages
         pages.erase(victim->id);
+
+        // remove node
         delete victim;
       }
 
+      // check whether botten-freq exist,
+      // if not, create new freq
       if (freqs.count(1) == 0) {
         struct Node *mru = nullptr, *lru = nullptr;
         freqs.insert(std::pair<int, std::pair<struct Node *, struct Node *>>(
             1, std::pair<struct Node *, struct Node *>(mru, lru)));
       }
 
+      // add node to botten-freq
       auto freq = freqs.find(1);
+      // create node
       struct Node *node = new struct Node;
       node->id = *page;
       node->prev = nullptr;
       node->next = nullptr;
       if (freq->second.first == nullptr && freq->second.second == nullptr) {
+        // add new node
         freq->second.first = node;
         freq->second.second = node;
       } else {
+        // add node to head (MRU)
         freq->second.first->prev = node;
         node->next = freq->second.first;
         freq->second.first = freq->second.first->prev;
       }
+
+      // add node to pages
       pages.insert(std::pair<int, std::pair<int, struct Node *>>(
           *page, std::pair<int, struct Node *>(1, node)));
     }
@@ -119,40 +148,58 @@ void LRU(std::vector<int> &history, int nframe) {
     auto search = pages.find(*page);
     if (search != pages.end()) {
       hit++;
+
+      // page is head (MRU): skip
       if (search->second == mru) {
         continue;
       }
+
       if (search->second == lru) {
+        // page is tail (LRU)
         lru = lru->prev;
         lru->next = nullptr;
       } else {
+        // page is mid of list
         search->second->prev->next = search->second->next;
         search->second->next->prev = search->second->prev;
       }
+
+      // move page to head (MRU)
       mru->prev = search->second;
       search->second->next = mru;
       mru = mru->prev;
     } else {
       miss++;
+
+      // if frame if full
       if (pages.size() == nframe) {
         struct Node *victim = lru;
         lru = lru->prev;
         lru->next = nullptr;
         pages.erase(victim->id);
+
+        // remove node
         delete victim;
       }
+
+      // add node to list
+      // create node
       struct Node *node = new struct Node;
       node->id = *page;
       node->prev = nullptr;
       node->next = nullptr;
       if (mru == nullptr && lru == nullptr) {
+        // add new node
         mru = node;
         lru = node;
       } else {
+        // add node to head (MRU)
         mru->prev = node;
         node->next = mru;
         mru = mru->prev;
       }
+
+      // add node to pages
       pages.insert(std::pair<int, struct Node *>(*page, node));
     }
   }
@@ -170,6 +217,7 @@ int main(int argc, char **argv) {
     while (infile >> page)
       history.push_back(page);
 
+    // LFU
     auto lfu_start = std::chrono::high_resolution_clock::now();
     std::cout << "LFU policy:\n";
     std::cout << "Frame\tHit\t\tMiss\t\tPage fault ratio\n";
@@ -184,6 +232,7 @@ int main(int argc, char **argv) {
 
     std::cout << '\n';
 
+    // LRU
     auto lru_start = std::chrono::high_resolution_clock::now();
     std::cout << "LRU policy:\n";
     std::cout << "Frame\tHit\t\tMiss\t\tPage fault ratio\n";
