50a6d5fb1c5c0c9b16c5ffb5a025fcb32451dbf6
finch185277 <59487andy@gmail.com>
2019-12-21 14:52:13 +0800

    Update main.cpp

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -13,14 +13,43 @@ public GitHub repository or a public web page.
 #include <string.h>
 
 int my_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
-               off_t offset, struct fuse_file_info *fi) { /*do something*/
+               off_t offset, struct fuse_file_info *fi) {
+  if (strcmp(path, "/") != 0)
+    return -ENOENT;
+
+  filler(buffer, ".", NULL, 0);
+  filler(buffer, "..", NULL, 0);
+  filler(buffer, path + 1, NULL, 0);
+
+  return 0;
 }
 
-int my_getattr(const char *path, struct stat *st) { /*do something*/
+int my_getattr(const char *path, struct stat *st) {
+  st->st_uid = getuid();
+  st->st_gid = getgid();
+  st->st_atime = time(NULL);
+  st->st_mtime = time(NULL);
+
+  if (strcmp(path, "/") == 0) {
+    st->st_mode = S_IFDIR | 0755;
+    st->st_nlink = 2;
+  } else {
+    st->st_mode = S_IFREG | 0644;
+    st->nlink = 1;
+    st->st_size = 1024;
+  }
+
+  return 0;
 }
 
 int my_read(const char *path, char *buffer, size_t size, off_t offset,
-            struct fuse_file_info *fi) { /*do something*/
+            struct fuse_file_info *fi) {
+  if (size < 5) {
+    memcpy(buffer, "Hello", size);
+    return size;
+  } else {
+    return -1;
+  }
 }
 
 static struct fuse_operations op;
