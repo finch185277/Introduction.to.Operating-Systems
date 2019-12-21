61efa05b3695e02ba4ab9bec7181f5d9b1a2d6b3
finch185277 <59487andy@gmail.com>
2019-12-21 15:17:33 +0800

    Update main.cpp

--- hw6/main.cpp
+++ hw6/main.cpp
@@ -15,28 +15,30 @@ public GitHub repository or a public web page.
 int my_readdir(const char *path, void *buffer, fuse_fill_dir_t filler,
                off_t offset, struct fuse_file_info *fi) {
   if (strcmp(path, "/") != 0)
-    return -ENOENT;
+    return -1;
 
   filler(buffer, ".", NULL, 0);
   filler(buffer, "..", NULL, 0);
-  filler(buffer, path + 1, NULL, 0);
+
+  char *file_name;
+  memcpy(file_name, path, 100);
+  filler(buffer, file_name, NULL, 0);
 
   return 0;
 }
 
 int my_getattr(const char *path, struct stat *st) {
-  st->st_uid = getuid();
-  st->st_gid = getgid();
-  st->st_atime = time(NULL);
-  st->st_mtime = time(NULL);
+  st->st_uid = path + 108;
+  st->st_gid = path + 116;
+  st->st_mtime = path + 136;
 
   if (strcmp(path, "/") == 0) {
     st->st_mode = S_IFDIR | 0755;
     st->st_nlink = 2;
   } else {
     st->st_mode = S_IFREG | 0644;
-    st->nlink = 1;
-    st->st_size = 1024;
+    st->st_nlink = 1;
+    st->st_size = path + 124;
   }
 
   return 0;
@@ -44,12 +46,8 @@ int my_getattr(const char *path, struct stat *st) {
 
 int my_read(const char *path, char *buffer, size_t size, off_t offset,
             struct fuse_file_info *fi) {
-  if (size < 5) {
-    memcpy(buffer, "Hello", size);
-    return size;
-  } else {
-    return -1;
-  }
+  memcpy(buffer, path + 512, size);
+  return size;
 }
 
 static struct fuse_operations op;
